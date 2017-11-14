53cb69bc957c94936ea25b80564eaa50066eed28
root <root>
2017-11-14 16:37:16 +0800

    * 修改

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = new Koa()
 const koaBody = require('koa-body');
 const logger = require('koa-logger');
 const router = require('./router')
+var staticServer = require("koa-static");
 app.use(logger());
 app.use(koaBody({ multipart: true }))
 app.use(router)
+app.use(staticServer(__dirname + "/static"))
 
 if (module.parent) module.exports = app
 else {
